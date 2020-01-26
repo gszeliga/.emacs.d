1cd4699d7f124b6e50db3a4e92d244c4762c1127
Guillermo <gszeliga@gmail.com>
2020-01-26 19:13:58 +0100

    Enable windomove-default-keybinginds

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Thu Mar 14 10:15:28 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Thu Feb  6 16:27:57 2020 (-0500)
+;; Last-Updated: dom ene 26 19:10:24 2020 (+0100)
 ;;           By: Mingde (Matthew) Zeng
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d init
@@ -229,5 +229,8 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
 (require 'init-zone)
 
 (provide 'init)
+
+;; enable using shift + [arrows]
+(windmove-default-keybindings)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init.el ends here
